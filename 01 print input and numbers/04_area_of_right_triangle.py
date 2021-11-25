ca8cee6c4bacd27a18fa1d9661821aeb0e00ee57
Eugenius Mario S <72474469+eugeniusms@users.noreply.github.com>
2021-11-25 18:46:21 +0700

    Create 04_area_of_right_triangle.py

---
+++ 01 print input and numbers/04_area_of_right_triangle.py
@@ -0,0 +1,6 @@
+#gunakan variabel alas dan tinggi untuk mempermudah pembacaan
+alas = int(input())
+tinggi = int(input())
+
+#untuk mencari luas segitiga dapat membagi dua bagian segi empat tersebut (square/2)
+print(alas*tinggi/2)
