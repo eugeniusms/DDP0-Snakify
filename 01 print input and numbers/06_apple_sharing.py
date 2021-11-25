39b6373e8e1fa395d9f6576696aeba81bc99789a
Eugenius Mario S <72474469+eugeniusms@users.noreply.github.com>
2021-11-25 18:47:27 +0700

    Create 06_apple_sharing.py

---
+++ 01 print input and numbers/06_apple_sharing.py
@@ -0,0 +1,7 @@
+#Membuat 2 variabel pembagi dan terbagi
+pembagi = int(input())
+terbagi = int(input())
+
+#membuat operasi floor division dan modulo
+print(terbagi//pembagi)
+print(terbagi%pembagi)
